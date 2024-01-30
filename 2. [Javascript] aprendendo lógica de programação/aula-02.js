2035b74c4c4edab71207f0554c77c2a43873b361
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-01-30 02:03:17 -0300

    2. [Javascript] aprendendo lógica de programação AULA 4 CONST/LET

---
+++ 2. [Javascript] aprendendo lógica de programação/aula-02.js
@@ -0,0 +1,9 @@
+//der preferência a o CONST
+const numero1 = 1 //CONST uma constante, não pode ser modificado, fica mais leve.
+let numero2 = 2; //LET pode ser alterado.
+
+if (numero1 === 1) {
+    numero2 = 6;
+}
+
+console.log("Resultado: ", numero2);
