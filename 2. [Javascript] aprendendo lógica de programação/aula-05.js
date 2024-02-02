c0826b392c66a8f395c14e3616826734ef9f1c44
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-02-02 20:52:29 -0300

    6. Como utilizar o FOR

---
+++ 2. [Javascript] aprendendo lógica de programação/aula-05.js
@@ -0,0 +1,8 @@
+let idade = 0;
+
+
+while (idade < 18) {
+    console.log("Idade antes do LOOP: ", idade) //começa no 0, termina no 17
+    idade += 1;
+    console.log("Idade depois do LOOP: ", idade) //começa no 1, termina no 18
+}
